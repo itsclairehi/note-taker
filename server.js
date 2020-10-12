50717266008687506fa1c983dcf0362da9eb2bbb
claire p <cpuckett9@gmail.com>
2020-10-11 21:41:38 -0500

    * route working

--- server.js
+++ server.js
@@ -31,15 +31,14 @@ app.get('/notes', (req, res)=> {
     res.sendFile(path.join(__dirname, './public/notes.html'));
   })
 
-  // app.get('*', (req, res)=> {
-  //   res.sendFile(path.join(__dirname, './public/index.html'));
-  // })
-
   app.get('/api/notes', (req, res)=> {
     
    return res.json(notes)
   })
 
+  app.get('*', (req, res)=> {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  })
   //stores new note
   app.post('/api/notes', (req, res)=> {
       
